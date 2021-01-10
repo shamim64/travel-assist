584ece93849a8a3bcf1e51f8f041b1ca49f9329f
shamim64 <ahmed.shamim@northsouth.edu>
2021-01-10 20:01:13 +0600

    Added a file status.php

---
+++ status.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Status</title>
+</head>
+
+<body>
+
+<?php
+
+	$cn=makeconnection();
+	$s="update enquiry set statusfield='Confirm' where enquiryid='" . $_GET["eid"] . "'";
+	mysqli_query($cn,$s);
+	mysqli_close($cn);
+header("location:viewenquiry.php");
+?>
+</body>
+</html>
