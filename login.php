b3638544287a04f5748cf15712adc354177c063c
shamim64 <ahmed.shamim@northsouth.edu>
2021-01-10 19:57:53 +0600

    Added a file login.php

---
+++ login.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login Form</title>
+
+<link rel="stylesheet" type ="text/css" href="css/style.css">
+
+</head>
+<body>
+  
+  <div class = "header">
+
+    <h2><center>Login</center></h2>
+  </div>
+  <center>
+  <div class="bg-img">
+
+  <form method= "post" action = "login.php" >
+
+    <table border="5" width = "350px" height = "300px">
+      <tr>
+        <td colspan="2" align = "center"></td>
+      </tr>
+      <tr>
+        <td>User Name</td><td><input type= "text"/></td>
+      </tr>
+      <tr>
+        <td>Pasword</td> <td><input type = "password"/></td>
+      </tr>
+
+
+      <tr>
+        <td colspan="2" align="center"><input type="button" value="Login" /></td>
+      </tr>
+  
+  </table>
+  
+ 
+</form>
+</div>
+ <p>
+  Not yet a member? <a href="register.php">Sign up</a>
+  </p>  
+</center>
+</body>
+</html>
