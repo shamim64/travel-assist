3c065354768cf0249de3b88a1d9f13b8b413903a
shamim64 <ahmed.shamim@northsouth.edu>
2021-01-10 19:55:25 +0600

    Added a file homepage.php

---
+++ homepage.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Home Page</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+  <?php echo 
+
+  ?>
+  <style type="text/css">
+    p{
+      font-size: 20px;
+      text-align: center;
+    }
+    h1{
+      text-align: center;
+      margin: 30px 0px;
+    }
+    h2{
+      font-size: 36px;
+      margin: 30px 0px;
+    }
+  </style>
+</head>
+<body>
+
+<br>
+
+<div class="container">
+  
+
+<div class="row">
+  <div class="col">
+    
+  </div>
+</div>
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <ul class="navbar-nav">
+    <li class="nav-item active">
+      <a class="nav-link" href="register.php">Register</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="updateplace.php">Update Place</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="viewplace">View Places</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link " href="status.php">Status</a>
+    </li>
+  </ul>
+</nav>
+
+</br>
+
+<picture>
+  <source media="(min-width: 650px)" srcset="images/t4.jpg">
+  <source media="(min-width: 650px)" srcset="images/3.jpg">
+  <img src="images/t4.jpg">
+  <img src="images/3.jpg">
+</picture>
+
+
+
+<h1>About </h1>
+  <p>To travel from one place to another in different purposes is generally called travelling. People do not travel always in the same purposes. Someone travels to gain knowledge and experience, someone travels for pleasure, someone travels for business purposes etc. It has much educative value. It is a part of education. Our education and book knowledge remain imperfect without travelling. We can learn many things by travelling. It teaches us trade and commerce, language, sociology, customs, culture, history, geography and so on.</p>
+ </br>
+</body>
+</html>
+
+
